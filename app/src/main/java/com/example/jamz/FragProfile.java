09093b30fe1b2b297d7fd0ae47ea650e73fccb8f
Gahouray <gdukuray@gmail.com>
2019-04-23 19:53:34 -0400

    Preferences Page

--- app/src/main/java/com/example/jamz/FragProfile.java
+++ app/src/main/java/com/example/jamz/FragProfile.java
@@ -2,22 +2,22 @@ package com.example.jamz;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class FragProfile extends Fragment {
 
-    public FragProfile(){
+    public FragProfile() {
         //Required empty public constructor
 
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         //Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
+
+
 }
